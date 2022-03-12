a55ff8cc908c1b364d1a1db003050c12a75e15fb
omar akki <omar.akki1@gmail.com>
2022-03-12 19:46:14 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div><h1>Welcom from react app</h1></div>
+  )
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+require('file-loader?name=[name].[ext]!./index.html');
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+import './App.scss';
+
+ReactDom.render(<App />, document.getElementById('app'));
